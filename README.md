c1fdc470e9289066ab0b629a2444099a1a060a96
Jason Hartley <jasonhartley@gmail.com>
2015-07-03 20:54:26 -0400

    added 15 second delay followed by reconnect to original wifi network.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # wifi-connect-poc
 ## Programmattically connect to a wifi access point
 
-Super simple proof-of-concept.
+Super simple proof-of-concept to programattically change the wifi network to one with a given SSID
+and password.  Then after 15 seconds, reconnect back to the original SSID, all with zero user input.
 
 Edit `res/values/strings.xml` to contain your SSID and password.  Assumes a connection to a WPA-protected AP.
--- app/src/main/java/us/jasonh/wificonnect/MainActivity.java
+++ app/src/main/java/us/jasonh/wificonnect/MainActivity.java
@@ -2,15 +2,23 @@ package us.jasonh.wificonnect;
 
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final ScheduledExecutorService worker =
+            Executors.newSingleThreadScheduledExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         wifiConfig.preSharedKey = "\""+ mohawkPass +"\"";
 
         WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int previousNetworkId = wifiInfo.getNetworkId();
+
         wifiManager.addNetwork(wifiConfig);
 
         List<WifiConfiguration> configuredNetworksList = wifiManager.getConfiguredNetworks();
@@ -37,9 +48,26 @@ public class MainActivity extends AppCompatActivity {
                 wifiManager.enableNetwork(configuredNetwork.networkId, true);
                 wifiManager.reconnect();
 
+                Toast.makeText(getApplicationContext(), "Connected to gadget wifi.",
+                        Toast.LENGTH_LONG).show();
+
                 break;
             }
         }
+
+        reconnectToPreviousNetwork(wifiManager, previousNetworkId);
+
+    }
+
+    void reconnectToPreviousNetwork(final WifiManager wifiManager, final int previousNetworkId) {
+        Runnable task = new Runnable() {
+            public void run() {
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(previousNetworkId, true);
+                wifiManager.reconnect();
+            }
+        };
+        worker.schedule(task, 15, TimeUnit.SECONDS);
     }
 
     @Override
